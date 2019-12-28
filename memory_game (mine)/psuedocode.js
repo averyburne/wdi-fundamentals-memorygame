fd47b30d7458b6d1e65c86e6caf980332dce004a
Avery <burneav@gmail.com>
2019-12-28 15:27:27 -0500

    adding HTML, CSS, and initial Javascript

---
+++ memory_game (mine)/psuedocode.js
@@ -0,0 +1,17 @@
+/*
+
+A person will look to buy a ticket
+Check to see patron's age
+If the patron is older than 65
+the price is $6
+If the patron is under 25 
+the price is $8
+Otherwise
+the price is $10
+
+
+
+
+
+
+*/
